2ca07b4be3ebb44db7ff10305a661e865a1add1e
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-11 14:40:25 -0600

    Initial Project Cleanup
    Created Initial project, Cleaned up boilerplate code, removed anything not needed for project

---
+++ EthereumCalculator/EthereumCalculator/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  EthereumCalculator
+//
+//  Created by Bradley GIlmore on 5/11/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+

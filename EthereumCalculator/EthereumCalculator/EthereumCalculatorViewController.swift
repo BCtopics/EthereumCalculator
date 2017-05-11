e28a8f01a4093177d840e29b0f92fceb9e62a0f2
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-11 14:50:06 -0600

    Add Views/ViewControllers. Implement Views
    Adds EthereumCalculatorViewcontroller and view. View setup.

---
+++ EthereumCalculator/EthereumCalculator/EthereumCalculatorViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EthereumCalculatorViewController.swift
+//  EthereumCalculator
+//
+//  Created by Bradley GIlmore on 5/11/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class EthereumCalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   
+    }
+    
+
+}
